ba2a1143315d68a2a32f8d09a3bccc54b36d8b11
Leonardo Goncalves <leomcg@gmail.com>
2021-04-17 17:06:43 -0300

    feat(Student): implementando JPA

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
+
 
 @SpringBootApplication
 
